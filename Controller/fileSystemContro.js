d538f29947525e134602d2d939bd8bcd5da86973
saurabhdeshmukh-98 <deshmukhsaurabh475@gmail.com>
2022-11-08 18:13:50 +0530

    modified

--- Controller/fileSystemContro.js
+++ Controller/fileSystemContro.js
@@ -19,7 +19,44 @@ const creatingFile = (req, res) => {
     });
   }
 };
-
+const readingFile = (req, res) => {
+  const { path } = req.body;
+  try {
+    const resp = fs.readFile(path);
+    res.status(200).json({
+      status: appConst.status.success,
+      response: resp,
+      message: "reading successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: appConst.status.fail,
+      response: null,
+      message: "sorry...some thing wrong ",
+    });
+  }
+};
+const shiftingFile = (req, res) => {
+  const { source, destination } = req.body;
+  try {
+    const resp = fs.rename(source, destination);
+    res.status(200).json({
+      status: appConst.status.success,
+      response: resp,
+      message: "it is shifted..",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: appConst.status.fail,
+      response: null,
+      message: "it is not shifted..",
+    });
+  }
+};
 module.exports = {
-  creatingFile
+  creatingFile,
+  readingFile,
+  shiftingFile
 };
--- router/router.js
+++ router/router.js
@@ -1,5 +1,8 @@
 const fileController = require("../Controller/fileSystemContro");
 const router = require("express").Router();
 router.post("/generateFile", fileController.creatingFile);
+router.get('/read',fileController.readingFile);
+router.post('/throw',fileController.shiftingFile);
+
 
 module.exports = router;
